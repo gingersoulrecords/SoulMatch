1e3fc0a25b9dcac55bbcc277abb23bd600cdfd96
Ginger Soul Records <gingersoulrecords@gmail.com>
2017-01-04 13:49:58 -0500

    re-commit

---
+++ soulmatch-admin.js
@@ -0,0 +1,35 @@
+// wp-admin scripts
+console.log( soulmatch_admin_data.options );
+
+jQuery(function($){
+	jQuery('.soulrepeater-delete').click(soulmatch.delete_repeater);
+	jQuery('.soulrepeater-add').click(soulmatch.add_repeater);
+});
+
+var soulmatch = {
+	delete_repeater : function(e){
+		console.log(e);
+		jQuery(this).parents('.postbox').remove();
+		e.preventDefault();
+	},
+	add_repeater : function(e){
+		jQuery.get(
+			ajaxurl,
+			{
+				'action' : 'add_repeater',
+			},
+			soulmatch.insert_repeater
+		);
+	},
+	insert_repeater : function( response ) {
+		if ( !response.success ){
+			console.log( response.data );
+			return;
+		}
+		jQuery('#post-body-content .meta-box-sortables').append( response.data );
+		jQuery('#post-body-content .meta-box-sortables .postbox').last().find('.soulrepeater-delete').click(soulmatch.delete_repeater);
+		jQuery('html, body').animate({
+	    scrollTop: jQuery('#post-body-content .meta-box-sortables .postbox').last().offset().top
+		}, 1000);
+	},
+}
---
+++ soulmatch.js
@@ -0,0 +1,21 @@
+// frontend scripts
+console.log( soulmatch_data.options );
+
+jQuery(function($){
+
+	$.each(soulmatch_data.options,function(i, value){
+	
+		$(value.selector).matchHeight({
+			byRow:value.byrow,
+		});	
+	
+	}
+	
+	);
+
+
+});
+
+
+
+
