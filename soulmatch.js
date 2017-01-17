950bdab647f9072b161329fd11766ce48ac608c9
Arūnas Liuiza <ask@arunas.co>
2017-01-17 17:18:20 +0200

    fix 0 !== false

--- soulmatch.js
+++ soulmatch.js
@@ -4,18 +4,14 @@ console.log( soulmatch_data.options );
 jQuery(function($){
 
 	$.each(soulmatch_data.options,function(i, value){
-	
+
 		$(value.selector).matchHeight({
-			byRow:value.byrow,
-		});	
-	
+			byRow: '0' === value.byrow ? false : true,
+		});
+
 	}
-	
+
 	);
 
 
 });
-
-
-
-
