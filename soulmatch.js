2c75db2a7f0b45ab208ceb3acf48f4d2011f9b60
Arūnas Liuiza <ask@arunas.co>
2017-04-11 16:33:10 +0300

    Add .filter(':visible")

--- soulmatch.js
+++ soulmatch.js
@@ -2,16 +2,9 @@
 //console.log( soulmatch_data.options );
 
 jQuery(function($){
-
 	$.each(soulmatch_data.options,function(i, value){
-
-		$(value.selector).matchHeight({
+		$(value.selector).filter(':visible').matchHeight({
 			byRow: '0' === value.byrow ? false : true,
 		});
-
-	}
-
-	);
-
-
+	});
 });
