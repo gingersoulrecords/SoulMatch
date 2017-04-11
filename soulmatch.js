d3d53a404bebb8b3bb0f6e1defcda9be9884c0b6
Arūnas Liuiza <ask@arunas.co>
2017-04-11 16:35:33 +0300

    reinitialize on resize()

--- soulmatch.js
+++ soulmatch.js
@@ -2,9 +2,13 @@
 //console.log( soulmatch_data.options );
 
 jQuery(function($){
-	$.each(soulmatch_data.options,function(i, value){
-		$(value.selector).filter(':visible').matchHeight({
-			byRow: '0' === value.byrow ? false : true,
+	function SoulMatch_init() {
+		$.each(soulmatch_data.options,function(i, value){
+			$(value.selector).filter(':visible').matchHeight({
+				byRow: '0' === value.byrow ? false : true,
+			});
 		});
-	});
+	}
+	jQuery( window ).resize( SoulMatch_init );
+	SoulMatch_init();
 });
